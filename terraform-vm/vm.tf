d0a14e80bd3d83074a1a07de1a415725f3ab4932
BerezinMN <Chaoos.dreadnought@gmail.com>
2023-09-24 17:23:20 +0400

    Add vm.tf and gitignore

--- terraform-vm/vm.tf
+++ terraform-vm/vm.tf
@@ -1,3 +1,28 @@
-resource "yandex_compute_instance" "vm-1" {
-  name = "terraform1"
+resource "yandex_compute_instance" "lamp" {
+  name        = "lamp"
+  platform_id = "standard-v2"
+  zone        = "ru-central1-a"
+
+  resources {
+    cores         = 2
+    memory        = 2
+    core_fraction = 5
+  }
+
+  boot_disk {
+    initialize_params {
+      image_id = "fd8dfofgv8k45mqv25nq"
+    }
+  }
+
+  network_interface {
+    subnet_id      = "e9b6uo6n7ieig6u0h6aj"
+    nat_ip_address = "true"
+  }
+
+  metadata = {
+    ssh-keys = "machine-key:${file("~/.ssh/id_rsa.pub")}"
+  }
 }
+
+
